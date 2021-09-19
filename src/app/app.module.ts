9ae82bcc02a67849f4743710f937f915d5c6113b
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-09-19 19:07:12 -0300

    Criação do comp Initial Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialPageComponent } from './pages/initial-page/initial-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialPageComponent
   ],
   imports: [
     BrowserModule,
