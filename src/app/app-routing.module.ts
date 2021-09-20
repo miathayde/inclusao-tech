41c0b3061e246cefe23db7b5c0863ddc04265a17
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-09-19 22:06:55 -0300

    Configurações iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitialPageComponent } from './pages/initial-page/initial-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InitialPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/initial-page/initial-page.component.ts
+++ src/app/pages/initial-page/initial-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialPageComponent implements OnInit {
 
+  initial: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
