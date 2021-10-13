ac0f51f777d3b5ecaf37b0d37aa6e691453c8716
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-10-13 19:56:17 -0300

    Ajustes de header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialPageComponent } from './pages/initial-page/initial-page.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialPageComponent
+    InitialPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
