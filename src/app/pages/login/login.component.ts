619f49e4b619c338db97f2b56d0f76514951fb82
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-10-31 20:44:52 -0300

    Criação tela de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  cpf: string;
+  senha: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  entrar() {
+    
+  }
 }
