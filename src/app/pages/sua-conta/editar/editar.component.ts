8c896cf76fb18dc7cfb1b56b6666ea6b1ec8ea9a
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-11-08 20:18:36 -0300

    Ajustes de css nas logos de formularios

--- src/app/pages/sua-conta/editar/editar.component.ts
+++ src/app/pages/sua-conta/editar/editar.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html',
-  styles: [
-  ]
+  styleUrls: ['./editar.component.scss']
 })
 export class EditarComponent implements OnInit {
+  modo: string;
+  id: number;
 
-  constructor() { }
+  senhaAtual: string;
+  novaSenha: string;
+  repetirSenha: string;
+  senhasIguais: string = "vazio";
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.modo = this.route.snapshot.paramMap.get('modo');
+    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    console.log(this.id)
   }
 
+  verificarSenha() {
+    if(this.novaSenha == "" && this.repetirSenha == "") {
+      this.senhasIguais = "vazio";
+    } else if(this.novaSenha != this.repetirSenha) {
+      this.senhasIguais = "diferentes";
+    } else if(this.novaSenha == this.repetirSenha) {
+      this.senhasIguais = "iguais";
+    }
+  }
 }
