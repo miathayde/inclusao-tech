d2911b1e3ad53d92b5537d4c641b2adf1b944f29
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-11-23 21:49:47 -0300

    botões direcionando de acordo com o id

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -10,17 +10,33 @@ import { filter } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
 
   mostrarSubmenu: boolean = true;
+  idCurso: number;
+  idUsuario: number;
+  rotaCursos: string;
+  rotaSuaConta: string;
 
   constructor(private router: Router) { 
     this.router.events.pipe(
       filter((event: any) => event instanceof NavigationEnd)
   )
       .subscribe(event => {
+        console.log(event)
           if(event.url == '/' || event.url == '/cadastro' || event.url == '/login') {
             this.mostrarSubmenu = false;
           } else {
             this.mostrarSubmenu = true;
           }
+
+          var url = event.url.replace(/[^0-9]/g, '');
+          if(url.length == 1) {
+            this.idUsuario = url;
+          } else {
+            this.idUsuario = url.substr(0,1);
+            this.idCurso = url.substr(-1,1);
+          }
+          
+          this.rotaCursos = `/lista-cursos/${this.idUsuario}`;
+          this.rotaSuaConta = `/sua-conta/${this.idUsuario}`;
       });
   }
 
