9d8012ae1b40e503e2ef8d3d8642c54cdf36a4c8
Michelle Athayde <michelle.athayde@intcom.com.br>
2021-11-27 01:49:52 -0300

    mostrando cursos disponiveis no lista-cursos

--- src/app/pages/lista-cursos/lista-cursos.component.ts
+++ src/app/pages/lista-cursos/lista-cursos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CursosService } from '../cursos.service';
+import { Usuario } from '../models/usuario';
+import { UsuariosService } from '../usuarios.service';
 
 @Component({
   selector: 'app-lista-cursos',
@@ -11,24 +13,43 @@ export class ListaCursosComponent implements OnInit {
 
   cursos: any;
   idUsuario: any;
+  usuario: Usuario = new Usuario();
 
   constructor(
-    private service: CursosService,
+    private cursosService: CursosService,
+    private usuarioservice: UsuariosService,
     private route: ActivatedRoute
   ) {
     this.route.params.subscribe(params => this.idUsuario = params['id']);
    }
 
   ngOnInit(): void {
-    this.listarCursos();
+    this.listarDadosUsuario();
+    this.listarNomeCursos();
   }
 
-  listarCursos() {
-    this.service.listarNomeCursos().subscribe(
+  listarNomeCursos() {
+    this.cursosService.listarNomeCursos().subscribe(
       result => {
         this.cursos = result;
       }
     )
+  }
+
+  listarDadosUsuario() {
+    this.usuarioservice.obterUsuario(this.idUsuario)
+    .subscribe(result => {
+      this.usuario = result;
+
+      this.cursos.forEach(e => {
+        if(e.id == 1) e.status = this.usuario.aula1;
+        else if(e.id == 2) e.status = this.usuario.aula2;
+        else if(e.id == 3) e.status = this.usuario.aula3;
+        else if(e.id == 4) e.status = this.usuario.aula4;
+        else if(e.id == 5) e.status = this.usuario.aula5;
+      });
+      console.log(this.cursos)
+    });
   }
 
   abrirCurso(id: number, nome: string) {
